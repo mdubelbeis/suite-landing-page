9ee1940aaaf7bcbd5548e77b3c5a4efc389a5d0a
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-20 00:07:49 -0400

    imported components

--- src/components/Info.component.js
+++ src/components/Info.component.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import Image from './Image.component';
+import Action from './Action.component';
+import Stats from './Stats.component';
+import Button from './Button.component';
+
 const Info = () => {
   return <div>Info</div>;
 };
