6dc354801bd97e227a09320ed70e7b7ff379f91b
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-20 01:13:29 -0400

    initial styling

--- src/components/Action.component.js
+++ src/components/Action.component.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
+import Arch from '../assets/pattern-curved-line-1.svg';
+
+import Button from './Button.component';
+
 const Action = () => {
-  return <div>Action</div>;
+  const [style, setStyle] = useState('action-btn');
+  return (
+    <div className="mt-14 flex flex-col">
+      <img
+        className="w-36 inline-block ml-auto mr-10"
+        src={Arch}
+        alt=""
+      />
+      <p className="text-[38px] text-left -tracking-[0.53px] mt-2">
+        A <span className="font-bold">super solution</span> for your
+        <span className="font-bold"> business.</span>
+      </p>
+      <p className="mt-8 mb-8 text-grey leading-7">
+        Our marketing and sales automations help you scale your
+        outreach to get more leads for your company.
+      </p>
+      <Button btnStyle={style} />
+    </div>
+  );
 };
 
 export default Action;
