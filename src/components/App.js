50c95f1030826d1e7a8b85645e25a782408b59f2
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-20 00:10:16 -0400

    initial styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,14 @@
+import Info from './Info.component';
+import Header from './Header.component';
+import Footer from './Footer.component';
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="container py-6 px-4">
+      <Header />
+      {/* <Footer /> */}
+    </div>
+  );
 };
 
 export default App;
