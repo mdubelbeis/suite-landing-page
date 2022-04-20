2bca8a6fda460d077193e154478aedfa9e3b49fb
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-20 01:12:54 -0400

    structured component

--- src/components/Info.component.js
+++ src/components/Info.component.js
@@ -3,10 +3,15 @@ import React from 'react';
 import Image from './Image.component';
 import Action from './Action.component';
 import Stats from './Stats.component';
-import Button from './Button.component';
 
 const Info = () => {
-  return <div>Info</div>;
+  return (
+    <div>
+      <Action />
+      <Image />
+      <Stats />
+    </div>
+  );
 };
 
 export default Info;
