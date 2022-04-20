0dfdfc5b7acb220f2a7ce0910f79847222027df1
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-20 00:06:05 -0400

    modified custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
       colors: {
         'dark-blue': '#172339',
         grey: '#49566D',
-        cream: '#F3EDE7',
-        'pure-white': '#FAF8F6',
+        'dark-cream': '#F3EDE7',
+        'light-cream': '#FAF8F6',
         'dull-purple': '#A060FF',
         'hot-pink': '#CB30E3',
         'dream-cicle': '#FFA84E',
