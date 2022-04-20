582ff94d1be238a705ae5c72a311e05cbf77e37a
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-19 23:02:35 -0400

    structured files

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="App"></div>;
+  return <div className="text-red-500">test</div>;
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-blue': '#172339',
+        grey: '#49566D',
+        cream: '#F3EDE7',
+        'pure-white': '#FAF8F6',
+        'dull-purple': '#A060FF',
+        'hot-pink': '#CB30E3',
+        'dream-cicle': '#FFA84E',
+      },
+    },
+  },
+  plugins: [],
+};
