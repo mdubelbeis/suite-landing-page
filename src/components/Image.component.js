920927429862e2de62a8511103a6b3782281cdaf
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-20 20:50:56 -0400

    implemented responsive images

--- src/components/Image.component.js
+++ src/components/Image.component.js
@@ -5,12 +5,35 @@ import Mobile2x from '../assets/images/image-hero-landscape@2x.png';
 import Tablet from '../assets/images/image-hero-portrait.png';
 import Desktop from '../assets/images/image-hero-portrait@2x.png';
 
-const images = [Mobile, Mobile2x, Tablet, Desktop];
-
 const Image = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [image, setImage] = useState(`${Mobile}`);
+
+  useEffect(() => {
+    const onLoad = () => {
+      setWindowWidth(window.innerWidth);
+
+      if (windowWidth <= 375) {
+        setImage(`${Mobile}`);
+      } else if (windowWidth < 768) {
+        setImage(`${Mobile2x}`);
+      } else if (windowWidth === 768) {
+        setImage(`${Tablet}`);
+      } else if (windowWidth > 768) {
+        setImage(`${Desktop}`);
+      }
+    };
+
+    window.addEventListener('resize', onLoad);
+    onLoad();
+
+    return () => window.removeEventListener('resize', onLoad);
+  }, [windowWidth]);
+
   return (
     <div>
-      <img src={Mobile} alt="logo" />
+      <img src={image} alt="logo" />
+      <p>{windowWidth}</p>
     </div>
   );
 };
