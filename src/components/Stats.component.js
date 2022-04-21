992c927e1db9cb5d22a86350b83336d9a3e9e668
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-20 21:07:37 -0400

    initial styling stats

--- src/components/Stats.component.js
+++ src/components/Stats.component.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <div className="text-center mt-12 space-y-6">
+      <div>
+        <h2>2K+</h2>
+        <p className="text-grey tracking-widest">COMPANIES</p>
+      </div>
+      <div>
+        <h2>8</h2>
+        <p className="text-grey tracking-widest">LANGUAGES</p>
+      </div>
+      <div>
+        <h2>1.2M</h2>
+        <p className="text-grey tracking-widest">LEADS</p>
+      </div>
+    </div>
+  );
 };
 
 export default Stats;
