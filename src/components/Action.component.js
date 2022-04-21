59cdcde00d7dce70fe9005564d41f17deb7c5f7f
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-20 22:09:18 -0400

    styled for tablet

--- src/components/Action.component.js
+++ src/components/Action.component.js
@@ -7,17 +7,17 @@ import Button from './Button.component';
 const Action = () => {
   const [style, setStyle] = useState('action-btn');
   return (
-    <div className="relative mt-14 flex flex-col px-4">
+    <div className="relative mt-14 flex flex-col px-4 md:w-9/12 md:mt-28">
       <img
-        className="absolute top-0 left-[172px] w-36 inline-block"
+        className="absolute top-0 left-[172px] w-36 inline-block md:w-56 md:left-[250px]"
         src={Arch}
         alt=""
       />
-      <p className="mt-6 text-[38px] text-left -tracking-[0.53px]">
+      <p className="mt-6 text-[38px] text-left -tracking-[0.53px] md:text-[56px] md:mt-10">
         A <span className="font-bold">super solution</span> for your
         <span className="font-bold"> business.</span>
       </p>
-      <p className="mt-8 mb-8 text-grey leading-7 tracking-normal">
+      <p className="mt-8 mb-8 text-grey leading-7 tracking-normal md:pr-48">
         Our marketing and sales automations help you scale your
         outreach to get more leads for your company.
       </p>
