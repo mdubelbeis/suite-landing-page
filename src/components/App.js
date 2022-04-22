d80ef2b195f03f57c8a31fbf2d8aa1c3c5263105
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-21 21:36:23 -0400

    corrected spacing

--- src/components/App.js
+++ src/components/App.js
@@ -8,10 +8,10 @@ const App = () => {
   return (
     <main>
       <Header />
-      <div className="w-auto mx-auto md:px-4 overflow-hidden max-w-[1109px]">
+      <div className="w-auto mx-auto md:px-4 overflow-hidden max-w-[1024px]">
         <Info />
       </div>
-      <div className="hidden lg:block md:absolute md:top-[1100px] md:-z-10 bg-dark-cream w-screen md:h-[600px]"></div>
+      <div className="hidden lg:block md:absolute md:top-[1100px] md:bottom-0 md:-z-10 bg-dark-cream w-screen md:h-[600px] min-h-full"></div>
       <Footer />
     </main>
   );
