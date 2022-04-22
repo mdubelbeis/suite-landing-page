797f50caf03bdef9cfedd98a4e26175333ffc8ba
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-21 20:53:23 -0400

    fixed accessibility, added sematic main

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,14 @@ import Footer from './Footer.component';
 
 const App = () => {
   return (
-    <div>
+    <main>
       <Header />
       <main className="w-auto mx-auto md:px-4 overflow-hidden max-w-[1109px] lg:py-10 lg:pb-0">
         <Info />
       </main>
       <div className="hidden lg:block md:absolute md:top-[1200px] md:-z-10 bg-dark-cream w-screen md:h-[500px]"></div>
       <Footer />
-    </div>
+    </main>
   );
 };
 
