f0b1e32aaf5849aff0151a58270865bf441434d1
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-21 23:03:24 -0400

    corrected height of bg element

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ const App = () => {
       <main className="w-auto mx-auto md:px-4 overflow-hidden max-w-[1109px]">
         <Info />
       </main>
-      <div className="hidden lg:block md:absolute md:top-[1100px] md:bottom-0 md:-z-10 bg-dark-cream w-screen md:h-full lg:h-[650px]"></div>
+      <div className="hidden lg:block md:absolute md:top-[1250px] md:bottom-0 md:-z-10 bg-dark-cream w-screen md:h-full lg:h-[500px]"></div>
       <Footer />
     </div>
   );
