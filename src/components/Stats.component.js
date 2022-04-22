d08e0923fa5d3d545666cc48ee996db57763cd9b
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-21 23:22:10 -0400

    stats text align left

--- src/components/Stats.component.js
+++ src/components/Stats.component.js
@@ -4,15 +4,15 @@ const Stats = () => {
   return (
     <div className="text-center mt-12 space-y-6  md:flex md:justify-around md:items-center md:object-fit md:space-y-0 md:mt-20 lg:flex-col lg:p-14 lg:items-start">
       <div>
-        <h2>2K+</h2>
+        <h2 className="lg:text-left">2K+</h2>
         <p className="text-grey tracking-widest">COMPANIES</p>
       </div>
       <div>
-        <h2>8</h2>
+        <h2 className="lg:text-left">8</h2>
         <p className="text-grey tracking-widest">LANGUAGES</p>
       </div>
       <div>
-        <h2>1.2M</h2>
+        <h2 className="lg:text-left">1.2M</h2>
         <p className="text-grey tracking-widest">LEADS</p>
       </div>
     </div>
