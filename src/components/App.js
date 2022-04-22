34ecefa5277adc2b2610ee8b2ff75109e16b6545
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-21 22:50:27 -0400

    fixing semantic elements

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,16 @@ import Footer from './Footer.component';
 
 const App = () => {
   return (
-    <main className="min-h-full">
-      <Header />
+    <div>
+      <header>
+        <Header />
+      </header>
       <div className="w-auto mx-auto md:px-4 overflow-hidden max-w-[1109px]">
         <Info />
       </div>
       <div className="hidden lg:block md:absolute md:top-[1100px] md:bottom-0 md:-z-10 bg-dark-cream w-screen md:h-full lg:h-[600px]"></div>
       <Footer />
-    </main>
+    </div>
   );
 };
 
